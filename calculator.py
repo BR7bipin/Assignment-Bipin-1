a3c082ae0b9a6c5335f0a56787e7945a70d1ea2f
Bipin Raut <bipinraut704@gmail.com>
2025-08-19 06:28:12 +0545

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+n1 = int(float(input("Enter first number: ")))
+n2 = int(float(input("Enter second number: ")))
+
+print(f"\nNumbers: {n1}, {n2}\n")
+
+print("Calculations:".upper())
+print("Addition:", n1 + n2)
+print("Substraction:", n1 - n2)
+print("Multiplication:", n1 * n2)
+print("Float Division:", n1 / n2)
+print("Integer Division:", n1 // n2)
+print("Modulus:", n1 % n2)
+print("Power:", n1 ** n2)
